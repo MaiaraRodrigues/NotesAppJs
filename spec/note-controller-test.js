185458cd76a88dd0b16bc3676e46cb00c72ea81f
charlie-galb <galbraith.charlie@gmail.com>
2020-11-12 17:05:54 +0000

    Notes now appear as link on page

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -1,12 +1,12 @@
-// 'use strict'; 
+// 'use strict';
 
 (function(){
-  var controller = new NoteController(); 
+  var controller = new NoteController();
 
   function canBeInstantiated(){
-    
+
     assert.isTrue(controller);
-  
+
 
   }
 
@@ -14,12 +14,18 @@
 
 
   function canChangeHtml(){
-    
 
-    assert.isTrue(document.getElementById('app').innerHTML === '<ul><li><div>Favourite drink: sel</div></li></ul>');
+    console.log(document.getElementById('app').innerHTML)
+    assert.isTrue(document.getElementById('app').innerHTML === '<ul><li><div><a id="0" href="#notes/0">Favourite drink: sel</a></div></li></ul>');
 
   }
 
+  function canAddNoteIDToURL() {
+    link = document.getElementsByTagName('a').click()
+    assert.isTrue(window.location.hash === '#notes/0')
+  }
+
   canChangeHtml();
+  canAddNoteIDToURL();
 
 })();
